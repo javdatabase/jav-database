f39622d9d095e7eb73605834d9b47e8671937963
NeroSs <minhdoan13051995@gmail.com>
2020-10-31 09:26:30 +0700

    Update Earning

--- src/services/earnings.service.js
+++ src/services/earnings.service.js
@@ -1,47 +1,47 @@
 function getEarningIdol(rank, points, styles, uncensored, best) {
-  const rankEarnings = Math.ceil(1000000 / rank);
+  const rankEarnings = Math.ceil(5000000 / rank);
 
-  let pointEarnings = 1000 * points;
+  let pointEarnings = 5000 * points;
   if (points >= 1000) {
-    pointEarnings = pointEarnings + 1000000;
+    pointEarnings = pointEarnings + 5000000;
   } else if (points < 1000 && points >= 500) {
-    pointEarnings = pointEarnings + 500000;
+    pointEarnings = pointEarnings + 2500000;
   } else if (points < 500 && points >= 200) {
-    pointEarnings = pointEarnings + 200000;
+    pointEarnings = pointEarnings + 1000000;
   } else if (points < 200 && points >= 100) {
-    pointEarnings = pointEarnings + 100000;
+    pointEarnings = pointEarnings + 500000;
   } else if (points < 100 && points >= 50) {
-    pointEarnings = pointEarnings + 50000;
+    pointEarnings = pointEarnings + 250000;
   } else if (points < 50 && points >= 20) {
-    pointEarnings = pointEarnings + 20000;
+    pointEarnings = pointEarnings + 100000;
   } else if (points < 20 && points >= 10) {
-    pointEarnings = pointEarnings + 10000;
+    pointEarnings = pointEarnings + 50000;
   } else if (points < 10 && points >= 5) {
-    pointEarnings = pointEarnings + 5000;
+    pointEarnings = pointEarnings + 25000;
   } else {
-    pointEarnings = pointEarnings + 1000;
+    pointEarnings = pointEarnings + 10000;
   }
 
-  let styleEarning = 100000;
+  let styleEarning = 500000;
   if (styles.find((item) => item.tag === "Retired")) {
-    styleEarning = 50000;
+    styleEarning = 100000;
   }
   if (styles.find((item) => item.tag === "6 Stars JAV")) {
-    styleEarning = styleEarning + 100000;
+    styleEarning = styleEarning + 500000;
   }
   if (styles.find((item) => item.tag === "Killer Tits")) {
-    styleEarning = styleEarning + 50000;
+    styleEarning = styleEarning + 500000;
   }
   if (styles.find((item) => item.tag === "Beautiful Breasts")) {
-    styleEarning = styleEarning + 50000;
+    styleEarning = styleEarning + 500000;
   }
 
-  const uncensoredEarnings = 5000 * uncensored;
+  const uncensoredEarnings = 10000 * uncensored;
 
   let bestEarnings = 0;
 
   if (best) {
-    bestEarnings = 1000000;
+    bestEarnings = 5000000;
   }
 
   return (
@@ -54,7 +54,7 @@ function getEarningIdol(rank, points, styles, uncensored, best) {
 }
 
 function getPriceOneNight(earnings) {
-  return Math.ceil(earnings / 3);
+  return Math.ceil(earnings / 5);
 }
 
 export { getEarningIdol, getPriceOneNight };
