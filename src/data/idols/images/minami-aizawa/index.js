c7b5f966e3f03cb6709aa54405d98666a69ed05b
vitditrakhoi <vitditrakhoi@gmail.com>
2021-01-10 23:16:56 +0700

    Update

---
+++ src/data/idols/images/minami-aizawa/index.js
@@ -0,0 +1,25 @@
+import minami_aizawa_1 from "./minami-aizawa-1.jpg";
+import minami_aizawa_2 from "./minami-aizawa-2.jpg";
+import minami_aizawa_3 from "./minami-aizawa-3.jpg";
+import minami_aizawa_4 from "./minami-aizawa-4.jpg";
+import minami_aizawa_5 from "./minami-aizawa-5.jpg";
+import minami_aizawa_6 from "./minami-aizawa-6.jpg";
+import minami_aizawa_7 from "./minami-aizawa-7.jpg";
+import minami_aizawa_8 from "./minami-aizawa-8.jpg";
+import minami_aizawa_9 from "./minami-aizawa-9.jpg";
+import minami_aizawa_10 from "./minami-aizawa-10.jpg";
+import minami_aizawa_11 from "./minami-aizawa-11.jpg";
+
+export {
+  minami_aizawa_1,
+  minami_aizawa_2,
+  minami_aizawa_3,
+  minami_aizawa_4,
+  minami_aizawa_5,
+  minami_aizawa_6,
+  minami_aizawa_7,
+  minami_aizawa_8,
+  minami_aizawa_9,
+  minami_aizawa_10,
+  minami_aizawa_11,
+};
