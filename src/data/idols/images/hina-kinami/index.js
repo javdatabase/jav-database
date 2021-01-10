9bb938f79184d008fb218304e7cfccedcdf865c3
vitditrakhoi <vitditrakhoi@gmail.com>
2021-01-10 23:06:01 +0700

    Update

---
+++ src/data/idols/images/hina-kinami/index.js
@@ -0,0 +1,17 @@
+import hina_kinami_1 from "./hina-kinami-1.jpg";
+import hina_kinami_2 from "./hina-kinami-2.png";
+import hina_kinami_3 from "./hina-kinami-3.jpg";
+import hina_kinami_4 from "./hina-kinami-4.jpg";
+import hina_kinami_5 from "./hina-kinami-5.jpg";
+import hina_kinami_6 from "./hina-kinami-6.jpg";
+import hina_kinami_7 from "./hina-kinami-7.jpg";
+
+export {
+  hina_kinami_1,
+  hina_kinami_2,
+  hina_kinami_3,
+  hina_kinami_4,
+  hina_kinami_5,
+  hina_kinami_6,
+  hina_kinami_7,
+};
