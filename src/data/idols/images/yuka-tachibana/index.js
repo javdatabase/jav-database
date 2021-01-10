2d4d6755509136490c12ed3d4686a76daf682ab6
vitditrakhoi <vitditrakhoi@gmail.com>
2021-01-11 00:19:41 +0700

    Update

---
+++ src/data/idols/images/yuka-tachibana/index.js
@@ -0,0 +1,23 @@
+import yuka_tachibana_1 from "./yuka-tachibana-1.jpg";
+import yuka_tachibana_2 from "./yuka-tachibana-2.jpg";
+import yuka_tachibana_3 from "./yuka-tachibana-3.jpg";
+import yuka_tachibana_4 from "./yuka-tachibana-4.jpg";
+import yuka_tachibana_5 from "./yuka-tachibana-5.jpg";
+import yuka_tachibana_6 from "./yuka-tachibana-6.jpg";
+import yuka_tachibana_7 from "./yuka-tachibana-7.jpg";
+import yuka_tachibana_8 from "./yuka-tachibana-8.jpg";
+import yuka_tachibana_9 from "./yuka-tachibana-9.jpg";
+import yuka_tachibana_10 from "./yuka-tachibana-10.jpg";
+
+export {
+  yuka_tachibana_1,
+  yuka_tachibana_2,
+  yuka_tachibana_3,
+  yuka_tachibana_4,
+  yuka_tachibana_5,
+  yuka_tachibana_6,
+  yuka_tachibana_7,
+  yuka_tachibana_8,
+  yuka_tachibana_9,
+  yuka_tachibana_10
+};
