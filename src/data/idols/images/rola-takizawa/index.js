b62c5a0088dbed08daad251f0911e939c8d192bd
vitditrakhoi <vitditrakhoi@gmail.com>
2021-01-10 23:25:37 +0700

    Update

---
+++ src/data/idols/images/rola-takizawa/index.js
@@ -0,0 +1,19 @@
+import rola_takizawa_1 from "./rola-takizawa-1.jpg";
+import rola_takizawa_2 from "./rola-takizawa-2.jpg";
+import rola_takizawa_3 from "./rola-takizawa-3.jpg";
+import rola_takizawa_4 from "./rola-takizawa-4.jpg";
+import rola_takizawa_5 from "./rola-takizawa-5.jpeg";
+import rola_takizawa_6 from "./rola-takizawa-6.jpg";
+import rola_takizawa_7 from "./rola-takizawa-7.jpg";
+import rola_takizawa_8 from "./rola-takizawa-8.jpeg";
+
+export {
+  rola_takizawa_1,
+  rola_takizawa_2,
+  rola_takizawa_3,
+  rola_takizawa_4,
+  rola_takizawa_5,
+  rola_takizawa_6,
+  rola_takizawa_7,
+  rola_takizawa_8,
+};
