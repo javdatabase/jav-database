f888cac85f36dcb0ea534e4d647825c54ff9aca9
vitditrakhoi <vitditrakhoi@gmail.com>
2021-01-10 23:02:20 +0700

    Update

---
+++ src/data/idols/images/erika-momotani/index.js
@@ -0,0 +1,25 @@
+import erika_momotani_1 from "./erika-momotani-1.jpg";
+import erika_momotani_2 from "./erika-momotani-2.jpg";
+import erika_momotani_3 from "./erika-momotani-3.jpg";
+import erika_momotani_4 from "./erika-momotani-4.jpg";
+import erika_momotani_5 from "./erika-momotani-5.jpg";
+import erika_momotani_6 from "./erika-momotani-6.jpg";
+import erika_momotani_7 from "./erika-momotani-7.jpg";
+import erika_momotani_8 from "./erika-momotani-8.jpg";
+import erika_momotani_9 from "./erika-momotani-9.jpg";
+import erika_momotani_10 from "./erika-momotani-10.jpg";
+import erika_momotani_11 from "./erika-momotani-11.jpg";
+
+export {
+    erika_momotani_1,
+    erika_momotani_2,
+    erika_momotani_3,
+    erika_momotani_4,
+    erika_momotani_5,
+    erika_momotani_6,
+    erika_momotani_7,
+    erika_momotani_8,
+    erika_momotani_9,
+    erika_momotani_10,
+    erika_momotani_11,
+};
