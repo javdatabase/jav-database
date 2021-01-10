e88ed94d2f8f526d29dda62d81bed491a53610db
vitditrakhoi <vitditrakhoi@gmail.com>
2021-01-10 23:27:33 +0700

    Update

---
+++ src/data/idols/images/satomi-ichihara/index.js
@@ -0,0 +1,17 @@
+import satomi_ichihara_1 from "./satomi-ichihara-1.jpg";
+import satomi_ichihara_2 from "./satomi-ichihara-2.jpg";
+import satomi_ichihara_3 from "./satomi-ichihara-3.jpg";
+import satomi_ichihara_4 from "./satomi-ichihara-4.jpg";
+import satomi_ichihara_5 from "./satomi-ichihara-5.jpg";
+import satomi_ichihara_6 from "./satomi-ichihara-6.jpg";
+import satomi_ichihara_7 from "./satomi-ichihara-7.jpg";
+
+export {
+  satomi_ichihara_1,
+  satomi_ichihara_2,
+  satomi_ichihara_3,
+  satomi_ichihara_4,
+  satomi_ichihara_5,
+  satomi_ichihara_6,
+  satomi_ichihara_7,
+};
