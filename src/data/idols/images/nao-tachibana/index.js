4e13e2563d588f32eb3c4f3f4138a576897520de
vitditrakhoi <vitditrakhoi@gmail.com>
2021-01-10 23:20:06 +0700

    Update

---
+++ src/data/idols/images/nao-tachibana/index.js
@@ -0,0 +1,23 @@
+import nao_tachibana_1 from "./nao-tachibana-1.png";
+import nao_tachibana_2 from "./nao-tachibana-2.jpg";
+import nao_tachibana_3 from "./nao-tachibana-3.jpg";
+import nao_tachibana_4 from "./nao-tachibana-4.jpg";
+import nao_tachibana_5 from "./nao-tachibana-5.jpg";
+import nao_tachibana_6 from "./nao-tachibana-6.jpg";
+import nao_tachibana_7 from "./nao-tachibana-7.jpg";
+import nao_tachibana_8 from "./nao-tachibana-8.jpg";
+import nao_tachibana_9 from "./nao-tachibana-9.jpg";
+import nao_tachibana_10 from "./nao-tachibana-10.jpg";
+
+export {
+  nao_tachibana_1,
+  nao_tachibana_2,
+  nao_tachibana_3,
+  nao_tachibana_4,
+  nao_tachibana_5,
+  nao_tachibana_6,
+  nao_tachibana_7,
+  nao_tachibana_8,
+  nao_tachibana_9,
+  nao_tachibana_10,
+};
