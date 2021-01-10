daa0b1411207f0a8cd7fdd99fb3ec78d231ba77a
vitditrakhoi <vitditrakhoi@gmail.com>
2021-01-10 22:57:13 +0700

    Update

---
+++ src/data/idols/images/arisa-shindo/index.js
@@ -0,0 +1,5 @@
+import arisa_shindo_1 from "./arisa-shindo-1.jpg";
+import arisa_shindo_2 from "./arisa-shindo-2.jpg";
+import arisa_shindo_3 from "./arisa-shindo-3.jpg";
+
+export { arisa_shindo_1, arisa_shindo_2, arisa_shindo_3 };
