a648718c9e724a6f3a130a6d084d03ce8c4ecda3
Doan Minh <minh.doan@vmodev.com>
2021-04-25 11:16:39 +0700

    Update

--- src/services/earnings.service.js
+++ src/services/earnings.service.js
@@ -1,47 +1,47 @@
 function getEarningIdol(rank, points, styles, uncensored, best) {
-  const rankEarnings = Math.ceil(30000000000 / rank);
+  const rankEarnings = Math.ceil(300000000 / rank);
 
-  let pointEarnings = 20000000 * points;
+  let pointEarnings = 200000 * points;
   if (points >= 1000) {
-    pointEarnings = pointEarnings + 1000000000;
-  } else if (points < 1000 && points >= 500) {
-    pointEarnings = pointEarnings + 600000000;
-  } else if (points < 500 && points >= 200) {
-    pointEarnings = pointEarnings + 300000000;
-  } else if (points < 200 && points >= 100) {
-    pointEarnings = pointEarnings + 100000000;
-  } else if (points < 100 && points >= 50) {
-    pointEarnings = pointEarnings + 90000000;
-  } else if (points < 50 && points >= 20) {
-    pointEarnings = pointEarnings + 30000000;
-  } else if (points < 20 && points >= 10) {
-    pointEarnings = pointEarnings + 1800000;
-  } else if (points < 10 && points >= 5) {
-    pointEarnings = pointEarnings + 16000000;
-  } else {
     pointEarnings = pointEarnings + 10000000;
+  } else if (points < 1000 && points >= 500) {
+    pointEarnings = pointEarnings + 6000000;
+  } else if (points < 500 && points >= 200) {
+    pointEarnings = pointEarnings + 3000000;
+  } else if (points < 200 && points >= 100) {
+    pointEarnings = pointEarnings + 1000000;
+  } else if (points < 100 && points >= 50) {
+    pointEarnings = pointEarnings + 900000;
+  } else if (points < 50 && points >= 20) {
+    pointEarnings = pointEarnings + 300000;
+  } else if (points < 20 && points >= 10) {
+    pointEarnings = pointEarnings + 180000;
+  } else if (points < 10 && points >= 5) {
+    pointEarnings = pointEarnings + 160000;
+  } else {
+    pointEarnings = pointEarnings + 100000;
   }
 
-  let styleEarning = 90000000;
+  let styleEarning = 900000;
   if (styles.find((item) => item.tag === "Retired")) {
-    styleEarning = 30000000;
+    styleEarning = 300000;
   }
   if (styles.find((item) => item.tag === "6 Stars JAV")) {
-    styleEarning = styleEarning + 100000000;
+    styleEarning = styleEarning + 1000000;
   }
   if (styles.find((item) => item.tag === "Killer Tits")) {
-    styleEarning = styleEarning + 60000000;
+    styleEarning = styleEarning + 600000;
   }
   if (styles.find((item) => item.tag === "Beautiful Breasts")) {
-    styleEarning = styleEarning + 60000000;
+    styleEarning = styleEarning + 600000;
   }
 
-  const uncensoredEarnings = 100000000 * uncensored;
+  const uncensoredEarnings = 1000000 * uncensored;
 
   let bestEarnings = 0;
 
   if (best) {
-    bestEarnings = 900000000;
+    bestEarnings = 9000000;
   }
 
   return (
@@ -54,7 +54,7 @@ function getEarningIdol(rank, points, styles, uncensored, best) {
 }
 
 function getPriceOneNight(earnings) {
-  return Math.ceil(earnings / 10);
+  return Math.ceil(earnings / 8);
 }
 
 const STAR_IDOL_ID = "jai158";
@@ -111,31 +111,31 @@ function getBonusEarnings(id) {
   let bonus = 0;
 
   if (STAR_IDOL_ID === id) {
-    bonus = bonus + 10000000000;
+    bonus = bonus + 100000000;
   }
   if (BEST_DVD_IDS.includes(id)) {
-    bonus = bonus + 1200000000;
+    bonus = bonus + 12000000;
   }
   if (BEST_IDOL_IDS.includes(id)) {
-    bonus = bonus + 600000000;
+    bonus = bonus + 6000000;
   }
   if (SWEET_NIGHT_IDS.includes(id)) {
     bonus =
       bonus +
-      1000000000 *
+      10000000 *
         SWEET_NIGHT_IDS.reduce((acc, val) => (val === id ? acc + 1 : acc), 0);
   }
   if (TOP_POINT_GROWTH_IDS.includes(id)) {
-    bonus = bonus + 180000000;
+    bonus = bonus + 1800000;
   }
   if (TOP_IDOL_IDS.includes(id)) {
-    bonus = bonus + 80000000;
+    bonus = bonus + 800000;
   }
   if (HAS_DVD_IDS.includes(id)) {
-    bonus = bonus + 50000000;
+    bonus = bonus + 500000;
   }
   if (NEWBIE_IDS.includes(id)) {
-    bonus = bonus + 6000000;
+    bonus = bonus + 120000;
   }
 
   return bonus;
