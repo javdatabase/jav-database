15aed18b3f778610569a09bc7558d3d19bc06fcd
Doan Minh <minh.doan@vmodev.com>
2021-06-06 01:05:59 +0700

    Update

--- src/services/earnings.service.js
+++ src/services/earnings.service.js
@@ -26,7 +26,7 @@ const BEST_IDOL_IDS = [
   "jai179",
   "jai187",
 ];
-const SWEET_NIGHT_IDS = ["jai001", "jai002", "jai002", "jai179"];
+const SWEET_NIGHT_IDS = ["jai001", "jai002", "jai002", "jai170", "jai179"];
 const TOP_POINT_GROWTH_IDS = ["jai001", "jai002", "jai048", "jai185"];
 const TOP_IDOL_IDS = [
   "jai062",
