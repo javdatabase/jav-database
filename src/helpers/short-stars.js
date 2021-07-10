aaaea991141d924f064135ada97dde9ffc9b9155
Doan Minh <minh.doan@vmodev.com>
2021-07-10 16:19:53 +0700

    Update

---
+++ src/helpers/short-stars.js
@@ -0,0 +1,13 @@
+const ShortStars = [
+  { idStar: "ups001", name: "Gabbie Carter" },
+  { idStar: "ups002", name: "Melody Marks" },
+  { idStar: "ups003", name: "Skylar Vox" },
+  { idStar: "ups004", name: "Leah Gotti" },
+  { idStar: "ups005", name: "Dolly Leigh" },
+  { idStar: "ups006", name: "Malina Mars" },
+  { idStar: "ups007", name: "Aubrey Gold" },
+  { idStar: "ups008", name: "Lily Jordan" },
+  { idStar: "ups009", name: "Abigaile Johnson" },
+];
+
+export default ShortStars;
