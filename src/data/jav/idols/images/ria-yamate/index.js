872a4c3b140c939c7b14e5f348b43d3e39631345
Doan Minh <minh.doan@vmodev.com>
2022-04-03 16:31:43 +0700

    Update

---
+++ src/data/jav/idols/images/ria-yamate/index.js
@@ -0,0 +1,25 @@
+import ria_yamate_1 from "./ria-yamate-1.jpeg";
+import ria_yamate_2 from "./ria-yamate-2.jpeg";
+import ria_yamate_3 from "./ria-yamate-3.jpeg";
+import ria_yamate_4 from "./ria-yamate-4.jpeg";
+import ria_yamate_5 from "./ria-yamate-5.jpeg";
+import ria_yamate_6 from "./ria-yamate-6.jpeg";
+import ria_yamate_7 from "./ria-yamate-7.jpeg";
+import ria_yamate_8 from "./ria-yamate-8.jpeg";
+import ria_yamate_9 from "./ria-yamate-9.jpeg";
+import ria_yamate_10 from "./ria-yamate-10.jpeg";
+import ria_yamate_11 from "./ria-yamate-11.jpeg";
+
+export {
+  ria_yamate_1,
+  ria_yamate_2,
+  ria_yamate_3,
+  ria_yamate_4,
+  ria_yamate_5,
+  ria_yamate_6,
+  ria_yamate_7,
+  ria_yamate_8,
+  ria_yamate_9,
+  ria_yamate_10,
+  ria_yamate_11,
+};
