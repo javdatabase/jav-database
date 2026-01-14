772359403d97a4d7d64eee0e701367bc162b0bd4
Doan Minh <minh.doan@vmodev.com>
2026-01-14 14:50:32 +0700

    Upddate

--- src/components/Dvds/DvdDetail.js
+++ src/components/Dvds/DvdDetail.js
@@ -144,7 +144,7 @@ function DvdDetail({ show, toggleModal, data }) {
   const timer = useRef();
 
   const link = useMemo(() => {
-    return `https://vlxx.bz/${get(
+    return `https://vlxx.net/${get(
       VIDEO_CONTENT(get(data, "code", "")),
       "xid",
       ""
